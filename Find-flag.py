bad6cec7c890f12d7ef3741c2182caed59915789
kodcdangky <78985628+kodcdangky@users.noreply.github.com>
2022-01-13 21:06:55 +0700

    Added User-Agent string to GET requests
    Fixed failed to get image problem

--- Find-flag.py
+++ Find-flag.py
@@ -1,8 +1,6 @@
 from tkinter import *
 from tkinter.ttk import Combobox
-
-import PIL
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk, UnidentifiedImageError
 from io import BytesIO
 import requests
 from bs4 import BeautifulSoup
@@ -12,15 +10,34 @@ root.minsize(320, 21)
 root.title('Flag Finder')
 root.resizable(0, 0)
 
-window = Frame()
-window.pack(fill='both')
-window.rowconfigure((0, 2), weight=1, minsize=21)
-window.columnconfigure(0, weight=0)
-window.columnconfigure(1, weight=1)
+header = Frame(
+    master=root,
+    bg='light gray'
+)
+header.columnconfigure(0, weight=0)
+header.columnconfigure(1, weight=1)
+header.pack(fill='x', side='top')
+
+body = Frame(
+    master=root,
+    bg='light gray'
+)
+body.rowconfigure((0, 2), weight=1)
+body.columnconfigure(0, weight=1)
+body.pack(fill='x', side='top')
+
+footer = Frame(
+    master=root,
+    bg='light gray'
+)
+footer.columnconfigure(0, weight=0)
+footer.columnconfigure(1, weight=1)
+footer.pack(fill='x', side='bottom')
 
 lbl_country = Label(
-    master=window,
+    master=header,
     text='Choose a country:',
+    bg='light gray'
 )
 lbl_country.grid(row=0, column=0, sticky='nw', padx=5)
 
@@ -222,23 +239,32 @@ countries = ('Afghanistan',
              'Zambia',
              'Zimbabwe')
 mnu_countries = Combobox(
-    master=window,
+    master=header,
     values=countries,
     state='readonly',
 )
-mnu_countries.grid(row=0, column=1, sticky='new', padx= 5)
+mnu_countries.grid(row=0, column=1, sticky='new', padx=5)
 
-def fetch_flag(country):
-    country = country.split(' ')
+
+def show_flag(Event):
+    try:
+        footer.grid_slaves(column=0)[0].grid_forget()
+        footer.grid_slaves(column=1)[0].grid_forget()
+        body.grid_slaves(row=0)[0].grid_forget()
+        body.grid_slaves(row=1)[0].grid_forget()
+        body.grid_slaves(row=2)[0].grid_forget()
+    except IndexError:
+        pass
+
+    country = mnu_countries.get().split(' ')
     flag_file = 'Flag_of'
     for name in country:
         flag_file += '_' + name
     flag_file += '.svg'
 
     link_wiki = 'https://en.wikipedia.org/wiki/File:' + flag_file
-    re = requests.get(link_wiki, timeout= 1)
-    if re.status_code != requests.codes.ok:
-        raise re.raise_for_status()
+    re = requests.get(link_wiki, timeout=1, headers={'User-Agent' : 'Mozilla/5.0 (nguyen6626nam@gmail.com) (Windows NT 10.0; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0'})
+    re.raise_for_status()
 
     page = BeautifulSoup(re.content, 'lxml')
     for meta in page('meta'):
@@ -247,47 +273,68 @@ def fetch_flag(country):
             break
 
     try:
-        img = Image.open(BytesIO(requests.get(link_image).content))
-        img = img.resize((img.width // 2, img.height // 2), Image.ANTIALIAS)
+        re = requests.get(link_image, timeout=1, headers={'User-Agent' : 'Mozilla/5.0 (nguyen6626nam@gmail.com) (Windows NT 10.0; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0'})
+        re.raise_for_status()
+        img = Image.open(BytesIO(re.content))
+        img = img.resize((img.width * 8 // 10, img.height * 8 // 10), Image.ANTIALIAS)
         img = ImageTk.PhotoImage(img)
         img_flag = Label(
-            master=window,
-            image=img
+            master=body,
+            image=img,
+            bg='light gray',
         )
         img_flag.image = img  # what the fuck is this sorcery https://stackoverflow.com/a/34235165
-        return img_flag, link_wiki
-    except PIL.UnidentifiedImageError:
-        lbl_fetch_error = Label(
-            master=window,
-            text='Failed to fetch the flag for this country. For now restarting the program fixes the problem.\nCurrently looking for a solution.',
+        img_flag.grid(rowspan=3, column=0, sticky='news')
+    except requests.exceptions.HTTPError:
+        lbl_error_msg0 = Label(
+            master=body,
+            bg='light gray',
+            text='Failed to fetch the flag for this country. For now going to the image\'s source:',
             justify='center'
         )
-        return lbl_fetch_error, link_wiki
 
-def show_flag(Event):
-    try:
-        try:
-            window.grid_slaves(row=1)[0].grid_forget()
-            window.grid_slaves(row=2)[0].grid_forget()
-        except IndexError:
-            pass
-
-        lbl_display, link_wiki = fetch_flag(mnu_countries.get())
-        lbl_display.grid(row=1, columnspan=2, sticky='news')
-
-        lbl_credit = Label(
-            master=window,
-            text='Photos fetched from the Wikimedia Commons in real time. Source: ' + link_wiki
+        lbl_error_link = Text(
+            master=body,
+            bg='light gray',
+            borderwidth=0,
+            height=1,
+            width=len(link_image),
+            font=('Segoe UI', 9)
         )
-        lbl_credit.grid(row=2, columnspan=2, sticky='news')
+        lbl_error_link.tag_config('center', justify=CENTER)
+        lbl_error_link.insert(0.0, link_image, 'center')
+        lbl_error_link.configure(state='disabled')
 
-    except FileNotFoundError:
-        try:
-            window.grid_slaves(row=1)[0].grid_forget()
-            window.grid_slaves(row=2)[0].grid_forget()
+        lbl_error_msg1 = Label(
+            master=body,
+            bg='light gray',
+            text='on your browser then reselecting the country fixes the problem (in most cases). Currently looking for a solution.',
+            justify='center'
+        )
+        lbl_error_msg0.grid(row=0, column=0, sticky='n')
+        lbl_error_link.grid(row=1, column=0)
+        lbl_error_msg1.grid(row=2, column=0)
 
-        except IndexError:
-            pass
+
+    lbl_credit = Label(
+        master=footer,
+        text='Photos fetched from the Wikimedia Commons in real time. Source: ',
+        bg='light gray'
+    )
+    lbl_credit_link = Text(
+        master=footer,
+        bg='light gray',
+        borderwidth=0,
+        height=1,
+        width=len(link_wiki),
+        font=('Segoe UI', 9)
+    )
+    lbl_credit_link.insert(0.0, link_wiki)
+    lbl_credit_link.configure(state='disabled')
+
+    lbl_credit.grid(row=0, column=0, sticky='ws')
+    lbl_credit_link.grid(row=0, column=1, sticky='ews', pady=1.5)
+
 
 mnu_countries.bind('<<ComboboxSelected>>', show_flag)
 
